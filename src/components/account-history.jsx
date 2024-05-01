4e4bde2c4561e67954deee77ebe7da583e2cdba9
Anthony Hall <apexa-antkky@outlook.com>
2024-05-01 01:48:06 -0700

    dashboard

---
+++ src/components/account-history.jsx
@@ -0,0 +1,50 @@
+import React, { useRef } from "react";
+import history from "./styles/account-history.module.scss";
+
+export default function Account_history() {
+    const transactions = [
+        {
+            _id: "66295c3e913e0c0cea6bae8f",
+            accountID: "66295be2913e0c0cea6bae84",
+            Type: "withdrawal",
+            Amount: 5000,
+            Method: "ethereum",
+            Confirmed: false,
+            __v: 0,
+        },
+        {
+            _id: "66295c29913e0c0cea6bae89",
+            accountID: "66295be2913e0c0cea6bae84",
+            Type: "deposit",
+            Amount: 5000,
+            Method: "wire-transfer",
+            Confirmed: false,
+            __v: 0,
+        },
+    ];
+
+    return (
+        <div className={history.content}>
+            {transactions.map((transactions) => {
+                return (
+                    <div className={history.item} key={transactions._id}>
+                        <label className={history.type}>
+                            {transactions.Type}
+                        </label>
+                        <label className={history.method}>
+                            {transactions.Method}
+                        </label>
+                        <div className={history.amountWrapper}>
+                            <label>
+                                {transactions.Amount.toLocaleString()}$
+                            </label>
+                        </div>
+                        <div className={history.dateWrapper}>
+                            <label>yesterday</label>
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,7 +1,80 @@
+import StatsModule from "../components/stats-module";
+import page from "../styles/dashboard.module.scss";
+import deposit from "../assets/deposit.svg";
+import money from "../assets/money.svg";
+import withdrawal from "../assets/withdrawal.svg";
+import Template from "./template";
+import EquityChart from "../components/EquityChart";
+import Account_history from "../components/account-history";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"; // remove for production
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useEffect } from "react";
+
 export default function DashboardPage() {
-    return (
-        <>
-            <></>
-        </>
-    );
+    const [cookies, setCookie] = useCookies(["name"]);
+
+    const { data, status, error } = useQuery({
+        queryKey: ["user"],
+        queryFn: async () => {
+            const response = await axios({
+                method: "get",
+                url: "/userData",
+                headers: {
+                    Authorization: "Bearer " + cookies.JWT,
+                },
+            });
+            return response.data;
+        },
+        staleTime: 1000 * 60,
+    });
+
+    if (status === "success") {
+        return (
+            <>
+                <div className={page.Content}>
+                    <section className={page.Welcome}>
+                        <div className={page.WelcomeText}>
+                            <h1 className={page.WelcomeTexth1}>
+                                Welcome Back,
+                            </h1>
+                            <h1 className={page.name}>{data.firstName}</h1>
+                        </div>
+                        <h1 className={page.missed}>Here's what you missed.</h1>
+                    </section>
+                    <hr className={page.rounded} />
+                    <section className={page.Stats}>
+                        <StatsModule
+                            amount={data.totalDeposits}
+                            label="total deposit"
+                            image={deposit}
+                        />
+                        <StatsModule
+                            amount={data.netProfit}
+                            label="Current Equity"
+                            image={money}
+                        />
+                        <StatsModule
+                            amount={data.totalWithdrawals}
+                            label="total withdrawals"
+                            image={withdrawal}
+                        />
+                    </section>
+                    <h1 className={page.sectionHeaders}>Account Equity</h1>
+                    <section className={page.EquityChart}>
+                        <EquityChart />
+                    </section>
+                    <h1 className={page.sectionHeaders}>Transaction History</h1>
+                    <section className={page.History}>
+                        <Account_history />
+                    </section>
+                    <section className={page.Footer}></section>
+                </div>
+                <ReactQueryDevtools /> {/* remove for prduction */}
+            </>
+        );
+    }
 }
