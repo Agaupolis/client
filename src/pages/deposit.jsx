8fb083d26e09a753b43ef14ddbe2a0ab3cdd21d0
Anthony Hall <apexa-antkky@outlook.com>
2024-05-01 02:51:39 -0700

    bruh

--- src/pages/deposit.jsx
+++ src/pages/deposit.jsx
@@ -1,7 +1,25 @@
+import Login from "../styles/login.module.scss";
+import deposit from "../assets/deposit.svg";
+import Input1 from "../components/Input1";
+
 export default function DepositPage() {
     return (
-        <>
-            <></>
-        </>
+        <div className={Login.Content}>
+            <div className={Login.Icon_Wrapper}>
+                <img src={deposit} />
+            </div>
+            <h1 className={Login.title}>Deposit</h1>
+            <form className={Login.Form}>
+                <div className={Login.Input}>
+                    <Input1 name="Method" type="text" />
+                </div>
+                <div className={Login.Input}>
+                    <Input1 name="Amount" type="number" />
+                </div>
+                <button className={Login.Button} type="submit">
+                    <p>send</p>
+                </button>
+            </form>
+        </div>
     );
 }
--- src/pages/withdrawal.jsx
+++ src/pages/withdrawal.jsx
@@ -1,7 +1,31 @@
+import Login from "../styles/login.module.scss";
+import withdrawal from "../assets/withdrawal.svg";
+import Input1 from "../components/Input1";
+
 export default function WithdrawalPage() {
     return (
-        <>
-            <></>
-        </>
+        <div className={Login.Content}>
+            {/* Key Icon */}
+            <div className={Login.Icon_Wrapper}>
+                <img src={withdrawal} />
+            </div>
+            {/* Authenticate Text */}
+            <h1 className={Login.title}>Withdrawal</h1>
+            <form className={Login.Form}>
+                {/* Username Input */}
+                <div className={Login.Input}>
+                    <Input1 name="Method" type="text" />
+                </div>
+                {/* Password Input */}
+                <div className={Login.Input}>
+                    <Input1 name="Amount" type="number" />
+                </div>
+                {/* Submit Button */}
+                <button className={Login.Button} type="submit">
+                    <p>send</p>
+                </button>
+                {/* End of Elements */}
+            </form>
+        </div>
     );
 }
