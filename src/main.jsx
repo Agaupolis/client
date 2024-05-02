4253192c955fe2a23435a501bcf99dda250005fd
Anthony Hall <apexa-antkky@outlook.com>
2024-05-02 03:27:10 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from "./App.jsx";
 import "./styles/index.scss";
 import { BrowserRouter as Router } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient({});
 ReactDOM.createRoot(document.getElementById("root")).render(
